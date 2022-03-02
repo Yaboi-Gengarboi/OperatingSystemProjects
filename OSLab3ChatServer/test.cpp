dfc26cb8b4cf100514029ad331803f0d8a66fec0
Justyn Durnford <jpdurnford@alaska.edu>
2022-03-01 21:28:08 -0900

    -Removed old demoClientUnix.c and demoServerUnix.c files.

---
+++ OSLab3ChatServer/test.cpp
@@ -0,0 +1,13 @@
+// OSLab3ChatServer
+// test.cpp
+// Created on 2022-02-25 by Justyn Durnford
+// Last modified on 2022-03-01 by Justyn Durnford
+
+#ifdef _WIN32
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
+#endif // #ifdef _WIN32

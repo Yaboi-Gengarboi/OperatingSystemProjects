c1a99b6b2efb9004b572745af581ccc88415fb70
Justyn Durnford <34521571+Yaboi-Gengarboi@users.noreply.github.com>
2022-03-04 20:16:36 -0900

    Add files via upload
    Reverted some changes.

--- OSLab3ChatServer/demoServer.cpp
+++ OSLab3ChatServer/demoServer.cpp
@@ -1,7 +1,7 @@
 // OSLab3ChatServer
 // demoServer.cpp
 // Downloaded from https://alaska.instructure.com/courses/6364/files/1441843?module_item_id=377680
-// Last modified on 2022-03-01 by Justyn Durnford and Dawson Hampton
+// Last modified on 2022-03-04 by Dawson Hampton and Justyn Durnford
 // Server side C++ program to demonstrate Socket programming.
 
 #if defined(__unix__)
@@ -9,102 +9,159 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <signal.h>
 
-#include <iostream>
-using std::cout;
-
+#include <cstddef>
 #include <functional>
-using std::ref;
-
+#include <iostream>
 #include <string>
-using std::string;
-
 #include <thread>
-using std::thread;
 
 #define PORT 8080
 
-bool hasClient1Sent, hasClient2Sent;
-string client1Str, client2Str;
+std::string One_to_Two;
+std::string Two_to_One;
+bool mail[2] = {0};
 
-void serverFunc(string& my_str, string& their_str)
+void sig_handler(int signo)
 {
-    int server_fd, new_socket, valread;
+    if (signo == SIGINT)
+        std::cout << "Received SIGINT\n";
+}
 
-    struct sockaddr_in address;
-
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    string buffer(1024, 0);
-    string reply;
-       
-    // Create socket file descriptor.
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        cout << "Socket creation failed.\n";
-        exit(EXIT_FAILURE);
-    }
+void sendMail(int new_socket, int position) 
+{
+    std::string myMessage;
+    std::string preface;
     
-    cout << "Socket creation successful.\n";
-       
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        cout << "setsockopt failed.\n";
-        exit(EXIT_FAILURE);
-    }
+    std::cout << position << " has sendMail running!\n";
     
-    cout << "setsockopt successful.\n";
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-       
-    // Forcefully attach socket to the port 8080.
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
-    {
-        cout << "Socket binding failed.\n";
-        exit(EXIT_FAILURE);
-    }
-    
-    cout << "Socket binding successful.\n";
-
-    // Listen for connections on a socket.
-    if (listen(server_fd, 3) < 0)
-    {
-        cout << "Listen failed.\n";
-        exit(EXIT_FAILURE);
-    }
-    
-    cout << "Listen successful.\n";
-
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
-    {
-        cout << "Acceptance error.\n";
-        exit(EXIT_FAILURE);
-    }
-    
-    cout << "Acceptance successful.\n";
-    
-    while (true)
-    {
-        sleep(1);
+    if (position == 0) 
+        std::string preface = "The message Client 0 sent was: ";
+    if (position == 1) 
+        std::string preface = "The message Client 1 sent was: ";
         
-        valread = read(new_socket, &buffer[0], 1024);
-        cout << buffer << '\n';
+    std::string reply;
+    
+    while (true) 
+    {
+        if (position == 0) 
+            myMessage = One_to_Two;
+        else 
+            myMessage = Two_to_One;
         
-        reply = string("The message you sent was: ") + buffer;
-        send(new_socket, reply.c_str(), reply.size(), 0);
-        cout << "Reply sent!\n";
+        if (mail[position] == true) 
+        {
+            mail[position] = false;
+            reply = preface + myMessage;
+            send(new_socket, reply.c_str(), reply.size(), 0);
+            std::cout << "Message sent!\n\n";
+        }
     }
 }
 
+void getMail(int new_socket, int position) 
+{
+    std::cout << position << " has getMail running!\n";
+    int valread;
+    std::string theirMessage;
+    std::string returnMessage = "Got the message!\n";
+    std::string buffer(1024, 0);
+    
+    while (true) 
+    {  
+        if (position == 1) 
+            theirMessage = One_to_Two;
+        else 
+            theirMessage = Two_to_One;
+            
+        for (int i = 0; i < 1024; ++i) 
+            buffer[i] = 0;
+        
+        valread = read(new_socket, buffer.data(), 1024);
+        
+        if (position == 1) 
+        {
+            One_to_Two = theirMessage; 
+            sleep(1); 
+            mail[0] = true;
+        }
+        else 
+        {
+            Two_to_One = theirMessage; 
+            sleep(1); 
+            mail[1] = true;
+        }
+        
+        std::cout << "Their message is: " << theirMessage << '\n';
+        send(new_socket, returnMessage.c_str(), returnMessage.size(), 0);
+    }
+}
+
+void ServerFunc(std::string myMessage, std::string theirMessage, int position, bool mail[2]) 
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    std::string buffer(1024, 0);
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    printf("%i is starting the threads!\n", position);
+    
+    std::thread get(getMail, new_socket, position);
+    std::thread send(sendMail, new_socket, position);
+    
+    get.join();
+    send.join();
+}
+    
 int main(int argc, char* argv[])
 {
-    thread firstUser(serverFunc, ref(client1Str), ref(client2Str));
-    thread secondUser(serverFunc, ref(client2Str), ref(client1Str));
-
+    if (signal(SIGINT, sig_handler) == SIG_ERR) 
+        std::cout << "\ncan't catch SIGINT\n";
+    
+    std::cout << "Starting the threads.\n";
+    std::thread firstUser(ServerFunc, One_to_Two, Two_to_One, 0, mail);
+    std::thread secondUser(ServerFunc, Two_to_One, One_to_Two, 1, mail);
+    
     firstUser.join();
+    secondUser.join();
     return 0;
 }
 
